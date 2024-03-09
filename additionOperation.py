4ee8ac12a72b28df31d13c828b37bdeb6fdb734f
meroo893 <84145091+meroo893@users.noreply.github.com>
2024-03-10 00:01:02 +0200

    CHANGED: -- Changed additionOperation into operations -- Changed the function idea for the operations -- Added operations for all the buttons
    -TODO:
    --Make generate label automatic in an array
    -- see if label, entry and button have common ancestor for easier object creation
    -- handle zeroDivisionError

--- additionOperation.py
+++
@@ -1,6 +0,0 @@
-﻿from getInput import getInput
-from updateGUI import updateGUI
-def addition(entry1_widget, entry2_widget):
-
-    result = int(getInput(entry1_widget)) + int(getInput(entry2_widget))
-    updateGUI(entry1_widget, entry2_widget, str(result))
---
+++ operations.py
@@ -0,0 +1,15 @@
+﻿from getInput import getInput
+from updateGUI import updateGUI
+def calculate(entry1_widget, entry2_widget, operand):
+    operand = operand.strip().lower()
+    left = float(getInput(entry1_widget))
+    right = float(getInput(entry2_widget))
+    operation = {
+        'addition': left + right,
+        'subtract': left - right,
+        'multiplication': left*right,
+        'division': left/right,
+    }
+    #TODO: handle zeroDivisionError
+    result = operation[operand]
+    updateGUI(entry1_widget, entry2_widget, str(result))
