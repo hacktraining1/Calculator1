ae7232ed78fb71f4712ec0effc954c13a3c5b0ed
meroo893 <84145091+meroo893@users.noreply.github.com>
2024-03-10 16:22:15 +0200

    Error window update
    better text and font and size and color

--- ErrorWindow.py
+++ ErrorWindow.py
@@ -4,7 +4,8 @@ from widgetFactory import generateWidget
 
 def throwError():
     error_window = generateMainWindow("Error!", "600x200")
-    label = generateWidget(error_window, "Label", text="Error! Cannot divide by 0!")
+    label = generateWidget(error_window, "Label", text="Error! Cannot divide by 0!".upper(), font=("Calibri", 36)
+                           ,foreground="red")
     label.pack(side="top", anchor="center", pady=20)
     button = generateWidget(error_window, "Button", text = "Close", command=lambda: error_window.destroy())
     button.pack(side="top", anchor="center", pady=20)
